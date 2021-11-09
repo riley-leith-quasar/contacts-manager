aa163a041cdf1f5d578b1428017153e03cfb10ea
Bosch Leith <bosch.leith@gmail.com>
2021-11-09 13:29:42 -0600

    Added addContact method

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,7 @@
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,72 +10,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Contact {
-    public String name;
-    public String number;
+	public String name;
+	public String number;
 
-    public Contact(String name, String number){
-        this.name = name;
-        this.number = number;
-    }
+	public Contact(String name, String number){
+		this.name = name;
+		this.number = number;
+	}
 
-    public static void LoadList(Path path) throws IOException {
-        System.out.println("Name  |   Phone Number");
-        System.out.println("----------------------");
+	public static void LoadList(Path path) throws IOException {
+		System.out.println("Name  |   Phone Number");
+		System.out.println("----------------------");
 
+		List<String> printList = Files.readAllLines(path);
+		for(int i = 0; i < printList.size(); i += 2){
+			System.out.println(printList.get(i) + " : " + printList.get(i+1));
+		}
+	}
 
+	public static void addContact(Path path) throws IOException {
+		Input input = new Input();
 
-        List<String> printList = Files.readAllLines(path);
-        for(int i = 0; i < printList.size(); i++){
-            System.out.println((i + 1) + ": " + printList.get(i));
-        }
+		System.out.println("Enter contact name: ");
+		String contactName = input.getString();
 
-        printList = Files.readAllLines(path);
-        List<String> newList = new ArrayList<>();
+		Contact test = new Contact("Test", "5555555");
 
-        for(String line: printList){
-            newList.add(line);
-        }
-        System.out.println(newList);
-        Files.write(path, newList);
-    }
+		List<String> contactList = Arrays.asList(test.name, test.number);
+		System.out.println("contactList = " + contactList);
+		System.out.println();
+		Files.write(path, contactList);
+	}
 
-    public static void addContact() throws IOException {
-        String directory = "./src/data";
-        String fileName = "contacts.txt";
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, fileName);
+	public static void main(String[] args) throws IOException {
+		String directory = "./src/data";
+		String fileName = "contacts.txt";
+		Path dataDirectory = Paths.get(directory);
+		Path dataFile = Paths.get(directory, fileName);
 
-        Contact test = new Contact("James", "5555555");
-//        List<Contact> contactList = Arrays.asList(test);
-        List<Contact> contactList1 = new ArrayList<>();
-        contactList1.add(test);
-        System.out.println("contactList = " + contactList1);
-
-//        Files.write(dataFile, contactList1);
-
-        //Reading from the file
-        List<String> printList = Files.readAllLines(dataFile);
-        System.out.println("printList = " + printList);
-
-        //We can write our own print method
-        for(int i = 0; i < printList.size(); i++){
-            System.out.println((i + 1) + ": " + printList.get(i));
-        }
-
-//        ArrayList<Contact> contactList = new ArrayList<>();
-//        List<String> contactList1 = contactList.
-//        contactList.add(new Contact("James", "555-5555"));
-//        for(Contact contact: contactList){
-//            System.out.println(contact);
-//        }
-
-//        Files.write(dataFile, (Iterable<? extends CharSequence>) contactList);
-//        List<Contact> contactList = List.of(test);
-//        Files.write(path, contactList);
-//        System.out.println(contactList);
-    }
-
-    public static void main(String[] args) throws IOException {
-        addContact();
-    }
+		addContact(dataFile);
+		LoadList(dataFile);
+	}
 }
---
+++ src/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Input {
+	private final Scanner scanner;
+
+	public Input(){
+		this.scanner = new Scanner(System.in);
+	}
+
+	public String getString(){
+		return this.scanner.nextLine();
+	}
+}
