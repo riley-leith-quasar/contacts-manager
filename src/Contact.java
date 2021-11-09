b762c9636e388321ab38fdbe52605a8840a2ac40
Bosch Leith <bosch.leith@gmail.com>
2021-11-09 16:39:03 -0600

    Added deleteContact method

--- src/Contact.java
+++ src/Contact.java
@@ -16,6 +16,22 @@ public class Contact {
 		this.number = number;
 	}
 
+	public static ArrayList<Contact> createContactsArray(Path path) throws IOException {
+		List<String> printList = Files.readAllLines(path);
+
+		ArrayList<Contact> contactArr = new ArrayList<>();
+
+		for(int i = 0; i < printList.size(); i += 2){
+			contactArr.add(new Contact(printList.get(i), printList.get(i+1)));
+		}
+
+		return contactArr;
+
+//		for (Contact contact : contactArr){
+//			System.out.printf("%s | %s%n", contact.name, contact.number);
+//		}
+	}
+
 	public static void LoadList(Path path, List<String> stringList) throws IOException {
 		System.out.println("Name  |   Phone Number");
 		System.out.println("----------------------");
@@ -26,9 +42,7 @@ public class Contact {
 //		}
 		for(int i = 0; i < stringList.size(); i += 2){
        System.out.println(stringList.get(i) + " : " + stringList.get(i+1));
-    }
-
-
+    	}
 	}
 
 	public static void addContact(Path path) throws IOException {
@@ -42,8 +56,8 @@ public class Contact {
 
 		ArrayList<Contact> contactArr = new ArrayList<>();
 		contactArr.add(new Contact(contactName, contactNumber));
-		for(int i = 0; i < contactArr.size(); i++){
-			List<String> contactList = Arrays.asList(contactArr.get(i).name, contactArr.get(i).number);
+		for (Contact contact : contactArr) {
+			List<String> contactList = Arrays.asList(contact.name, contact.number);
 			System.out.println("contactList = " + contactList);
 			Files.write(path, contactList, StandardOpenOption.APPEND);
 		}
@@ -54,23 +68,41 @@ public class Contact {
 	}
 
 	public static void searchContact(Path path, Input input) throws IOException {
+		ArrayList<Contact> contactArr = createContactsArray(path);
 		System.out.println("Enter contact name: ");
 		String userSearch = input.getString();
-		List<String> printList = Files.readAllLines(path);
-		System.out.println("printList.size() = " + printList.size());
-		for(int i = 0; i < printList.size(); i += 2){
-			System.out.println("i = " + i);
-			if(userSearch.equalsIgnoreCase(printList.get(i))) {
-				System.out.println(printList.get(i) + " : " + printList.get(i + 1));
+		boolean found = false;
 
-			} else {
-				System.out.printf("Contact %s not found%n", userSearch);
-				searchContact(path, input);
+		for (Contact contact : contactArr) {
+			if (userSearch.equalsIgnoreCase(contact.name)) {
+				System.out.printf("%s | %s%n", contact.name, contact.number);
+				found = true;
+				break;
 			}
 		}
-
-
+		if (!found){
+			System.out.printf("Contact %s not found%n", userSearch);
+			searchContact(path, input);
+		}
 	}
+
+	public static void deleteContact(Path path, Input input) throws IOException {
+		ArrayList<Contact> contactArr = createContactsArray(path);
+		System.out.println("Enter a contact: ");
+		String userSearch = input.getString();
+
+		List<String> newList = new ArrayList<>();
+
+		for (Contact contact : contactArr) {
+			if (userSearch.equalsIgnoreCase(contact.name)) {
+				continue;
+			}
+			newList.add(contact.name);
+			newList.add(contact.number);
+		}
+		Files.write(path, newList);
+	}
+
 	public static void main(String[] args) throws IOException {
 		Input input = new Input();
 		String directory = "./src/data";
@@ -78,8 +110,9 @@ public class Contact {
 		Path dataDirectory = Paths.get(directory);
 		Path dataFile = Paths.get(directory, fileName);
 
-//		addContact(dataFile);
+		addContact(dataFile);
 		System.out.println();
 		searchContact(dataFile, input);
+		deleteContact(dataFile, input);
 	}
 }
